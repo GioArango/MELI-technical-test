c3a3a25cb6355f224909230f80eb850d800092a9
giovarsu <giovanny.arango@cadena.com.co>
2024-02-11 19:14:26 -0500

    implementation of the application initiator file

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,11 +1,17 @@
+import { config } from "./config";
+import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
-(async() => {
+(async () => {
     main();
 })();
 
-function main(){
-    const app = new Server();
+function main() {
+    const app = new Server({
+        port: config.PORT,
+        public_path: config.PUBLIC_PATH,
+        routes: AppRoutes.routes
+    });
 
     app.start()
 }
