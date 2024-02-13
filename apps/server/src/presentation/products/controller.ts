bb81f5794181a5c7e98dc9d718ac944e3e84c495
giovarsu <giovanny.arango@cadena.com.co>
2024-02-12 23:14:21 -0500

    dependency injection and logic for data construction

--- apps/server/src/presentation/products/controller.ts
+++ apps/server/src/presentation/products/controller.ts
@@ -1,13 +1,56 @@
 import { Request, Response } from "express";
-import { getProductsByQuery } from "./services";
+import { ProductService } from './services';
+import { IProduct } from '../../models/Product';
 
 export class ProductController {
 
+    private readonly productServices: ProductService;
+    
+    constructor(productService: ProductService) {
+        this.productServices = productService
+    }
+    
     public getProducts = async (req: Request, res: Response) => {
-        const product = req.query.q;
+        try {
+            const product = req.query.q;    
+            const getProductsServiceResponse = await this.productServices.getProductsByQuery(product as string)
 
-        const serviceResponse = await getProductsByQuery(product as string)
+            // lógica que se moverá a otra capa    
+            
+            if(!getProductsServiceResponse.isSuccessful) {
+                throw new Error(getProductsServiceResponse.errorMessage)
+            }
 
-        res.status(200).json(serviceResponse)
+            const productsItems = [];
+
+            const productInfo = (getProductsServiceResponse.result as IProduct[]).map((product) => {
+                return {
+                    id: product.id,
+                    title: product.title,
+                    price: {
+                        currency: product.installments.currency_id,
+                        amount: product.installments.amount,
+                        decimals: 0 // https://api.mercadolibre.com/sites/MLA/currencies/${curency_id} -->
+                    },
+                    picture: product.thumbnail,
+                    condition: product.attributes.find(attribute => attribute.id === 'ITEM_CONDITION')?.value_name,
+                    free_shipping: product.shipping.free_shipping
+                }
+            })
+
+            console.log(productInfo)
+
+            productsItems.push(...productInfo)
+
+            let response = {
+                items: productsItems
+            }
+    
+            res.status(200).json(response)
+            
+        } catch (error) {
+            console.error(error)
+            res.status(500).json(error)
+        }
     }
 }
