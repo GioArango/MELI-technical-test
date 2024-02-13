4e5331ed4c3ab8172388c8971c6da9232871a83e
giovarsu <giovanny.arango@cadena.com.co>
2024-02-12 23:13:33 -0500

    implementation of product service class

--- apps/server/src/presentation/products/services.ts
+++ apps/server/src/presentation/products/services.ts
@@ -1,49 +1,57 @@
 import axios from "axios";
 import { GenericResponse } from "../../models/GenericResponse";
 
-export const getProductsByQuery = async (q: string): Promise<GenericResponse> => {
-    try {
-        const resp = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${q}&limit=4`)
-        console.log(resp)
-        return resp.data.results
-    } catch (error) {
-        console.error(error)
-        const resp: GenericResponse = {
-            isSuccessful: false,
-            errorMessage: error as string
+export class ProductService {
+
+    public getProductsByQuery = async (q: string): Promise<GenericResponse> => {
+        try {
+            const resp = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${q}&limit=4`)
+            
+            const serviceResponse: GenericResponse = {
+                isSuccessful: true,
+                result: resp.data.results
+            }
+            
+            return serviceResponse
+        } catch (error) {
+            console.error(error)
+            const serviceResponse: GenericResponse = {
+                isSuccessful: false,
+                errorMessage: error as string
+            }
+            return serviceResponse
         }
-        return resp
     }
-}
-
-export const getProductItemById = async (item: string): Promise<GenericResponse> => {
-    try {
-        const resp = await axios.get(`https://api.mercadolibre.com/items/${item}`)
-
-        return resp.data
-    } catch (error) {
-        console.error(error)
-
-        const resp: GenericResponse = {
-            isSuccessful: false,
-            errorMessage: error as string
+    
+    public getProductItemById = async (item: string): Promise<GenericResponse> => {
+        try {
+            const resp = await axios.get(`https://api.mercadolibre.com/items/${item}`)
+    
+            return resp.data
+        } catch (error) {
+            console.error(error)
+    
+            const resp: GenericResponse = {
+                isSuccessful: false,
+                errorMessage: error as string
+            }
+            return resp
         }
-        return resp
     }
-}
-
-export const getCategoriesByProductId = async(productId: string): Promise<GenericResponse> => {
-    try {
-        const resp = await axios.get(`https://api.mercadolibre.com/categories/${productId}`)
-
-        return resp.data;
-    } catch (error) {
-        console.error(error)
-
-        const resp: GenericResponse = {
-            isSuccessful: false,
-            errorMessage: error as string
+    
+    public getCategoriesByProductId = async(productId: string): Promise<GenericResponse> => {
+        try {
+            const resp = await axios.get(`https://api.mercadolibre.com/categories/${productId}`)
+    
+            return resp.data;
+        } catch (error) {
+            console.error(error)
+    
+            const resp: GenericResponse = {
+                isSuccessful: false,
+                errorMessage: error as string
+            }
+            return resp
         }
-        return resp
     }
 }
