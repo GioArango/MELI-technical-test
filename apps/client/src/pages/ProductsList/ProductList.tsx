77689329d498b24e7678bfc44c0c43f2c804dbb4
gioarango <gioarango.94@gmail.com>
2024-02-15 23:20:39 -0500

    initial design of the productList component

--- apps/client/src/pages/ProductsList/ProductList.tsx
+++ apps/client/src/pages/ProductsList/ProductList.tsx
@@ -1,7 +1,8 @@
+import { PATHS } from "@/constants";
 import { useAxiosFetch } from "@/hooks/useAxiosFetch";
 import { IProduct } from "@/interfaces";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const ProductList = () => {
 
@@ -33,13 +34,37 @@ const ProductList = () => {
 
       {
         (products && products?.items?.length > 0 && !loading) &&
-        <ul>
+        <>
           {
-            products?.items.map( item => (
-              <li>{item.title}</li>
+            products?.items.map((item, index) => (
+              <>
+                <div style={{ display: 'flex', flexDirection: 'row', width: '80vw', justifyContent: 'center', gap: 1, padding: 2 }}>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 5 }}>
+                    <Link to={`${PATHS.PRODUCTS}/${item.id}`}>
+                      <img src={item.picture} alt={item.title} style={{ marginInline: 30 }}/>
+                    </Link>
+                  </div>
+
+                  <div style={{ flexGrow: 1 }}>
+                    <div style={{ display: 'flex' }}>
+                      <p style={{ marginRight: 20 }}>{item.price.amount}</p> 
+                      <p>{item.free_shipping && 'envio gratis'}</p>
+                    </div>
+                    <span>{item.title}</span>
+                  </div>
+
+                  <div style={{ flexGrow: 1, textAlign: 'end', margin: 10 }}>
+                    <p>{"Ciudad"}</p>
+                  </div>
+                </div>
+                {
+                  (index + 1) !== products.items.length &&
+                  <hr style={{ marginInline: 10, backgroundColor: 'red' }}/>
+                }
+              </>
             ))
           }
-        </ul>
+        </>
       }
     </>
   )
