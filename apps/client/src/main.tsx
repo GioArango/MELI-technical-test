a7a0b1fc945ed206a4a762612f2d8d87d541b9bf
gioarango <gioarango.94@gmail.com>
2024-02-26 23:32:57 -0500

    add context provider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
+import { AppProvider } from './context/AppProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
+  <AppProvider>
     <App />
+  </AppProvider>
   // </React.StrictMode>,
 )
